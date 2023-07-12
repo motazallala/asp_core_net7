e3770d4b26dd49e66d7ceadc817805310e945b2a
motazallala <www.hot122@gmail.com>
2023-07-12 15:31:40 +0300

    add the department model to models.

---
+++ Coderz220/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Coderz220.Models
+{
+    public class Department
+    {
+
+        public int DepartmentId { get; set; }
+        [Required(ErrorMessage = "please Enter Department Name")]
+        [Display(Name = "Department Name")]
+        [MaxLength(10, ErrorMessage = "Max Length 10 Char")]
+        [MinLength(4, ErrorMessage = "Main length 4 Char")]
+        public string? DepartmentName { get; set; }
+    }
+}
